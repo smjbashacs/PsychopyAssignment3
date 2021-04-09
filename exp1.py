c7a4107ae9c0ef83bf96554205f03f3b0a9802ac
smjbashacs <57228693+smjbashacs@users.noreply.github.com>
2021-04-09 17:54:44 +0530

    adding experiment session and log file details

--- exp1.py
+++ exp1.py
@@ -26,3 +26,37 @@ from numpy.random import random, randint, normal, shuffle
 import os
 import sys
 from psychopy.hardware import keyboard
+
+
+# Ensure that relative paths start from the same directory as this script
+_thisDir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(_thisDir)
+
+# Store info about the experiment session - timestamp, participant info, psychopy tool info
+psychopyVersion = '2020.2.10'
+expName = 'exp1'  # from the Builder filename that created this script
+expInfo = {'participant': '', 'session': '001'}
+dlg = gui.DlgFromDict(dictionary=expInfo, sortKeys=False, title=expName)
+if dlg.OK == False:
+    core.quit()  # user pressed cancel
+expInfo['date'] = data.getDateStr()  # add a simple timestamp
+expInfo['expName'] = expName
+expInfo['psychopyVersion'] = psychopyVersion
+
+# Data file name stem = absolute path + name; later add .psyexp, .csv, .log, etc
+filename = _thisDir + os.sep + \
+    u'data/%s_%s_%s' % (expInfo['participant'], expName, expInfo['date'])
+
+# An ExperimentHandler isn't essential but helps with data saving
+thisExp = data.ExperimentHandler(name=expName, version='',
+                                 extraInfo=expInfo, runtimeInfo=None,
+                                 originPath='C:\\Users\\smjba\\Desktop\\psyexp\\exp1.py',
+                                 savePickle=True, saveWideText=True,
+                                 dataFileName=filename)
+# save a log file for detail verbose info
+logFile = logging.LogFile(filename+'.log', level=logging.EXP)
+# this outputs to the screen, not a file
+logging.console.setLevel(logging.WARNING)
+
+endExpNow = False  # flag for 'escape' or other condition => quit the exp
+frameTolerance = 0.001  # how close to onset before 'same' frame
